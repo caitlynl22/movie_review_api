a5e12ccf739ad8c52155ce087c9772163b1a3e42
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-03-06 11:29:21 -0500

    Add create action for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,19 @@ class ReviewsController < ApplicationController
     @reviews = @movie.reviews
     render json: @reviews, status: 200
   end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new(review_params)
+    @movie.reviews << @review
+    if @review.save
+      render json: @review, status: :created
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:user, :body, :rating)
+  end
 end

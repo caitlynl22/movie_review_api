5ca7ab6d95f97de343e19071e45a30e8ca5ba91c
Susan <susan@marpy.net>
2015-03-06 14:55:22 -0500

    added the admin namespace with update review function with testing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,9 @@
 class ReviewsController < ApplicationController
+
   def index
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
     render json: @reviews, status: 200
   end
+
 end
